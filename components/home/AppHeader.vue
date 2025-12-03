5bcebb67b78bd97852b62ed5bcc7b4d0f4f428de
faezeh ebrahimy <fezebr2000@gmail.com>
2025-12-04 00:41:01 +0330

    feat; add AppHeader

---
+++ components/home/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="mb-10 mt-2">
+    <h1
+      class="dark-text mb-2 text-2xl sm:text-3xl font-bold "
+    >
+      Real-time cryptocurrency market data
+    </h1>
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <p  class="bg-red-500">hiiiiiiiiiiiiiiii</p>
+  <div
+    class="min-h-screen bg-gray-800 dark-text overflow-hidden max-w-10xl mx-auto px-4 md:px-16"
+  >
+    <!-- <div class="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8"> -->
+      <AppHeader />
+    <!-- </div> -->
+  </div>
 </template>
 
-<script lang="ts">
+<script>
+import AppHeader from '../components/home/AppHeader.vue'
 
+export default {
+  components: {
+    AppHeader,
+  },
+}
 </script>
