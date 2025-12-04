693c43cac85df03843963ed40555354fadd0d677
faezeh ebrahimy <fezebr2000@gmail.com>
2025-12-05 01:27:20 +0330

    feat: add coin detail api

--- apis/coins.js
+++ apis/coins.js
@@ -2,3 +2,15 @@ export const getCoinsMarkets = (axios, params) => {
   return axios.$get('/coins/markets', { params })
 }
 
+export const getCoinDetails = (axios, coinId) => {
+  return axios.$get(`/coins/${coinId}`, {
+    params: {
+      localization: false,
+      tickers: false,
+      market_data: true,
+      community_data: false,
+      developer_data: false,
+      sparkline: false,
+    },
+  })
+}
