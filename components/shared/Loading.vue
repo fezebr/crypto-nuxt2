25b5d271fa2ff9a2ac632390a64fd6782eef7c87
faezeh ebrahimy <fezebr2000@gmail.com>
2025-12-05 01:25:27 +0330

    feat: add Loading component

---
+++ components/shared/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<style scoped>
+.spinner {
+  width: 40px;
+  height: 40px;
+  border: 3px solid #374151;
+  border-top-color: #3b82f6;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
