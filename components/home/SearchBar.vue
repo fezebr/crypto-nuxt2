f804332bb19957afe6ab459c585d6e216b3411fa
faezeh ebrahimy <fezebr2000@gmail.com>
2025-12-04 22:41:37 +0330

    feat: add searchbar to home page

---
+++ components/home/SearchBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="relative w-full md:w-96">
+    <svg
+      class="absolute left-4 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+      />
+    </svg>
+    <input
+      :value="value"
+      placeholder="Search..."
+      @input="onInput"
+      type="text"
+      class="w-full px-6 pl-12 py-4 rounded shadow text-sm dark-text placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 light-background"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    onInput(event) {
+      this.$emit('input', event.target.value)
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,28 @@
 <template>
-  <div
-    class="min-h-screen bg-gray-800 dark-text overflow-hidden max-w-10xl mx-auto px-4 md:px-16"
-  >
-    <!-- <div class="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8"> -->
-      <AppHeader />
-    <!-- </div> -->
+  <div class="px-4 md:px-16 py-8">
+    <SearchBar
+      v-model="search"
+      @input="onSearchInput"
+    />
   </div>
 </template>
 
 <script>
-import AppHeader from '../components/home/AppHeader.vue'
+import SearchBar from '~/components/home/SearchBar.vue'
 
 export default {
   components: {
-    AppHeader,
+    SearchBar
   },
+  data() {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+    onSearchInput() {
+    console.log(this.search)
+    }
+  }
 }
 </script>
