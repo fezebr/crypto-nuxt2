ce0631881693c54e984dfdd13bfbe85c8f34ab44
faezeh ebrahimy <fezebr2000@gmail.com>
2025-12-05 20:33:13 +0330

    feat: make loading full screen

--- components/shared/Loading.vue
+++ components/shared/Loading.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="flex items-center justify-center">
+  <div class="loading-overlay">
     <div class="spinner"></div>
   </div>
 </template>
 
 <style scoped>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(17, 24, 39, 0.95);
+  z-index: 9999;
+}
+
 .spinner {
-  width: 40px;
-  height: 40px;
-  border: 3px solid #374151;
+  width: 50px;
+  height: 50px;
+  border: 4px solid #374151;
   border-top-color: #3b82f6;
   border-radius: 50%;
   animation: spin 1s linear infinite;
